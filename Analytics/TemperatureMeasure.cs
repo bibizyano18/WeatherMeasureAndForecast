ca7213397bea64bef68e5998cfe1b2a4f0cb5d34
artem <artem@PA77>
2025-05-03 14:40:50 +0300

    Сделал класс, в котором будет содержаться расчёт температуры по дням

---
+++ Analytics/TemperatureMeasure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMeasureAndForecast.Analytics
+{
+    public class TemperatureMeasure
+    {
+        public List<decimal> temperature = new List<decimal>();
+
+        public decimal tempMeasure(decimal max, decimal min)
+        {
+            decimal result = Math.Abs(max) - Math.Abs(min);
+            return result;
+        }
+
+        // нужно добавить проход по массиву данных температур
+    }
+}
