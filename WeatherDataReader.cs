43a191291fba8f0cd3af33cea79913c776358311
Yarik <150702442+Yar4ee@users.noreply.github.com>
2025-05-04 14:33:51 +0300

    Создал класс для чтения информации из файла

---
+++ WeatherDataReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMeasureAndForecast
+{
+    public class WeatherDataReader
+    {
+        public static List<DataTemperature> LoadFromFile(string filePath)
+        {
+            
+        }
+    }
+}
