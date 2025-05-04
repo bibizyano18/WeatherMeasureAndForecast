36e12f5c62934ae0a7ca6d2d7f88af3491743b17
Yarik <150702442+Yar4ee@users.noreply.github.com>
2025-05-04 14:53:48 +0300

    Написал функционал чтения из файла

--- WeatherDataReader.cs
+++ WeatherDataReader.cs
@@ -1,8 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
+
+/* Пример файла (Дата, минимальная, максимальная, Прогноз)
+2025-04-01,2,10,Облачно
+2025 - 04 - 02,1,9,Ясно
+2025-04-03,3,12, Дождь
+2025-04-04,4,13, Облачно
+2025-04-05,5,14, Ясно */
 
 namespace WeatherMeasureAndForecast
 {
@@ -10,7 +21,34 @@ namespace WeatherMeasureAndForecast
     {
         public static List<DataTemperature> LoadFromFile(string filePath)
         {
+            var result = new List<DataTemperature>();
+            var lines = File.ReadAllLines(filePath);
             
+            for (int i = 0; i<lines.Length; i++)
+            {
+                var line = lines[i].Split(',');
+
+                if (line.Length < 4)
+                    continue;
+
+                try
+                {
+                    var day = new DataTemperature
+                    {
+                        Date = DateTime.Parse(line[0]),
+                        minTemp = double.Parse(line[1]),
+                        maxTemp = double.Parse(line[2]),
+                        Description = line[3]
+                    };
+                    result.Add(day);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка на строке {i+1}: {ex.Message}");
+                }
+            }
+            
+            return result;
         }
     }
 }
