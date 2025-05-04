81ef54bf412184426c12bce02d2ac99ad924a46b
Yarik <150702442+Yar4ee@users.noreply.github.com>
2025-05-04 14:26:48 +0300

    Добавил класс для структуры данных

---
+++ DataTemperature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMeasureAndForecast
+{
+    public class DataTemperature
+    {
+        public DateTime Date { get; set; }
+        public double maxTemp { get; set; }
+        public double minTemp { get; set; }
+        public string Description { get; set; }
+    }
+}
