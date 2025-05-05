ab7cc31b8788477e81b634b9a5fb588d2338db85
artem <artem@PA77>
2025-05-05 13:54:58 +0300

    Сделал рекурсивную функцию для расчёта экстраполяции. Пока на вход даю тестовый list, потом поменяю на значения, которые считываем с файла

--- Analytics/TemperatureForecast.cs
+++ Analytics/TemperatureForecast.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WeatherMeasureAndForecast.Analytics
 {
@@ -13,10 +14,24 @@ namespace WeatherMeasureAndForecast.Analytics
             15.5, 16.2, 13, 18.4, 19.2, 14.7
         };
 
-        public decimal TempForecast(decimal max, decimal min)
+        public void TempForecast(List<double> temps, List<double> res, int n)
         {
-            decimal result = Math.Abs(max) - Math.Abs(min);
-            return result;
+            if (n > temps.Count)
+            {
+                throw new ArgumentException("n превышает кол-во дней");
+            }
+            double sum = 0;
+            for (int i = temps.Count - 1; i >= temps.Count - n; i--)
+            {
+                sum += temps[i];
+            }
+            temps.Add(sum / n);
+            res.Add(sum / n);
+            if (res.Count == n)
+            {
+                return;
+            }
+            else { TempForecast(temps, res, n); }
         }
 
         // нужно добавить проход по массиву данных температур
